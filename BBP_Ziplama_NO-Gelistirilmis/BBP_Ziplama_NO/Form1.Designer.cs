911f37f2bf6aaa9ac2d26bdc572d640766cf77c7
osmankoyuncu53 <86400029+osmankoyuncu53@users.noreply.github.com>
2022-08-01 12:04:08 +0300

    Csharp Form + Zıplama Oyunu

---
+++ BBP_Ziplama_NO-Gelistirilmis/BBP_Ziplama_NO/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BBP_Ziplama_NO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ziplama = new System.Windows.Forms.Timer(this.components);
+            this.ilerleme = new System.Windows.Forms.Timer(this.components);
+            this.pb_oyuncu = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbl_zorluk = new System.Windows.Forms.Label();
+            this.lbl_puan = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.uretim = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_oyuncu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ziplama
+            // 
+            this.ziplama.Tick += new System.EventHandler(this.ziplama_Tick);
+            // 
+            // ilerleme
+            // 
+            this.ilerleme.Interval = 25;
+            this.ilerleme.Tick += new System.EventHandler(this.ilerleme_Tick);
+            // 
+            // pb_oyuncu
+            // 
+            this.pb_oyuncu.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pb_oyuncu.Location = new System.Drawing.Point(0, 105);
+            this.pb_oyuncu.Name = "pb_oyuncu";
+            this.pb_oyuncu.Size = new System.Drawing.Size(25, 25);
+            this.pb_oyuncu.TabIndex = 0;
+            this.pb_oyuncu.TabStop = false;
+            this.pb_oyuncu.Tag = "oyuncu";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.SkyBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(630, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 10);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "engel";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.pictureBox4.Location = new System.Drawing.Point(586, 14);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(15, 30);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "engel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 32);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Oyuna Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox5.Location = new System.Drawing.Point(0, 130);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(800, 50);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "zemin";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Crimson;
+            this.pictureBox3.Location = new System.Drawing.Point(521, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "engel";
+            // 
+            // lbl_zorluk
+            // 
+            this.lbl_zorluk.AutoSize = true;
+            this.lbl_zorluk.Location = new System.Drawing.Point(12, 14);
+            this.lbl_zorluk.Name = "lbl_zorluk";
+            this.lbl_zorluk.Size = new System.Drawing.Size(68, 17);
+            this.lbl_zorluk.TabIndex = 6;
+            this.lbl_zorluk.Text = "Zorluk : 1";
+            // 
+            // lbl_puan
+            // 
+            this.lbl_puan.AutoSize = true;
+            this.lbl_puan.Location = new System.Drawing.Point(106, 14);
+            this.lbl_puan.Name = "lbl_puan";
+            this.lbl_puan.Size = new System.Drawing.Size(61, 17);
+            this.lbl_puan.TabIndex = 7;
+            this.lbl_puan.Text = "Puan : 0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pb_oyuncu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 180);
+            this.panel1.TabIndex = 8;
+            // 
+            // uretim
+            // 
+            this.uretim.Interval = 1500;
+            this.uretim.Tick += new System.EventHandler(this.uretim_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 284);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl_puan);
+            this.Controls.Add(this.lbl_zorluk);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_oyuncu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer ziplama;
+        private System.Windows.Forms.Timer ilerleme;
+        private System.Windows.Forms.PictureBox pb_oyuncu;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbl_zorluk;
+        private System.Windows.Forms.Label lbl_puan;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer uretim;
+    }
+}
+
---
+++ BBP_Ziplama_NO-Gelistirilmis/BBP_Ziplama_NO/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBP_Ziplama_NO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool artis=true;
+        int sinir=1,ilkX,ilkY;
+        int sayac = 0;
+        int puan = 0;
+        int zorluk = 1;
+        int zorlukSayac = 0;
+        List<PictureBox> engel = new List<PictureBox>();
+        private PictureBox engelEkle(Color renk,int genislik, int yukseklik)
+        {
+            PictureBox nesne = new PictureBox();
+            nesne.Tag = "engel";
+            nesne.BackColor = renk;
+            nesne.Size = new Size(genislik, yukseklik);
+            engel.Add(nesne);
+            engel[sayac].Location = new Point(panel1.Width - engel[sayac].Width, pictureBox5.Location.Y - engel[sayac].Height);
+            panel1.Controls.Add(engel[sayac]);
+            return nesne;
+        }
+
+      
+        private PictureBox uret()
+        {
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(3);
+            switch (sayi)
+            {
+                case 0:
+                    return engelEkle(Color.Crimson,20,20);
+                    break;
+                case 1:
+                    return engelEkle(Color.MediumTurquoise, 15, 30);
+                    break;
+                case 2:
+                    return engelEkle(Color.SkyBlue, 50, 10);
+                    break;
+                default:
+                    return null;
+
+            }
+            
+        }
+        private void ziplama_Tick(object sender, EventArgs e)
+        {
+            if (sinir==5)
+            {
+                artis = false;
+                pb_oyuncu.Top -= pb_oyuncu.Height / 2;
+            }
+
+            if (artis)
+            {
+                pb_oyuncu.Top -= pb_oyuncu.Height / 2;
+                sinir++;
+            }
+            else
+            {
+                pb_oyuncu.Top += pb_oyuncu.Height / 2;
+                sinir--;
+            }
+
+            if (sinir == 0)
+            {
+                ziplama.Stop();
+            }
+        }
+
+       
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode==Keys.W || e.KeyCode==Keys.Space || e.KeyCode==Keys.Up) && ziplama.Enabled==false)
+            {
+                artis = true;
+                sinir = 1;
+                ziplama.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ilkX = pb_oyuncu.Left;
+            ilkY = pb_oyuncu.Top;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uretim_Tick(object sender, EventArgs e)
+        {
+            if (sayac < zorluk)
+            {
+                uret();
+                sayac++;
+            }          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pb_oyuncu.Location = new Point(ilkX, ilkY);
+            pb_oyuncu.Focus();
+
+            sayac = 0;
+            zorlukSayac = 0;
+            puan = 0;
+            zorluk = 1;
+            lbl_puan.Text = String.Format("Puan : {0}",puan);
+            lbl_zorluk.Text = String.Format("Zorluk : {0}", zorluk);
+            foreach (PictureBox pictureBox in panel1.Controls)
+            {
+                if (pictureBox.Tag.ToString() == "engel")
+                {
+                    panel1.Controls.Remove(pictureBox);
+                    engel.Clear();
+                }
+            }
+            uret();
+            ilerleme.Start();
+            button1.Enabled = false;
+        }
+
+
+        private void ilerleme_Tick(object sender, EventArgs e)
+        {
+
+            if (engel[0].Tag.ToString() == "engel")
+            {
+                if (engel[0].Bounds.IntersectsWith(pb_oyuncu.Bounds))
+                {
+                    ilerleme.Stop();
+                    ziplama.Stop();
+
+                    MessageBox.Show("Oyunu kaybettin :(");
+                    button1.Enabled = true;
+                }
+            }
+            if (sayac == 0)
+            {
+
+                uret();
+                sayac++;
+            }
+            else
+            {
+                if (engel[0].Right < 0)
+                {
+                    panel1.Controls.Remove(engel[0]);
+                    engel.RemoveAt(0);
+                    sayac--;
+                    puan++;
+                    zorlukSayac++;
+                    lbl_puan.Text = String.Format("Puan : {0}", puan);
+
+                    if (zorlukSayac == 2)
+                    {
+                        zorlukSayac = 0;
+                        zorluk++;
+                        lbl_zorluk.Text = String.Format("Zorluk : {0}", zorluk);
+                        uretim.Start();
+                    }
+
+                }
+                else
+                {
+                    for (int i = 0; i < engel.Count(); i++)
+                    {
+                        engel[i].Left -= (5 + zorluk);
+                    }
+                    
+                }
+
+            }
+
+        }
+    }
+}
